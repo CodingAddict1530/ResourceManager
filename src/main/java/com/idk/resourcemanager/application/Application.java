f3c424cf64027fcb4403943e20cfe27977b67c79
CodingAddict1530 <kejack2001@gmail.com>
2024-10-19 16:54:28 -0400

    Added changes

--- src/main/java/com/idk/resourcemanager/application/Application.java
+++ src/main/java/com/idk/resourcemanager/application/Application.java
@@ -1,7 +1,39 @@
 package com.idk.resourcemanager.application;
 
+import com.idk.resourcemanager.inspection.CodeInspector;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Application {
 
-    
+    private static final ArrayList<String> excludedPackages = new ArrayList<String>();
+    static {
+        excludedPackages.addAll(List.of("java.*", "javax.*", "jdk.*", "sun.*", "com.sun.*"));
+    }
+
+    public final void launch() {
+
+        CodeInspector.inspect();
+        run();
+
+    }
+
+    public abstract void run();
+
+    public static ArrayList<String> getExcludedPackages() {
+
+        return excludedPackages;
+    }
+
+    public static void addExcludedPackage(String p) {
+
+        excludedPackages.add(p);
+    }
+
+    public static void removeExcludedPackage(String p) {
+
+        excludedPackages.remove(p);
+    }
 
 }
