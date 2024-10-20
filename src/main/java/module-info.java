957cb49003b9ccb449016d6d35a5c4af406dca49
CodingAddict1530 <kejack2001@gmail.com>
2024-10-19 20:24:08 -0400

    Added ByteBuddy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,8 @@ module com.idk.resourcemanager {
     requires org.aspectj.weaver;
     requires io.github.classgraph;
     requires jdk.jdi;
+    requires java.compiler;
+    requires net.bytebuddy;
+    requires net.bytebuddy.agent;
 
 }
